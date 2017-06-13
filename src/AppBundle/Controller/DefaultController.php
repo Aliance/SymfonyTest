01ab51c28d6452e4ace21a0356f1469e7e585747
Lesnykh Ilia <lesnykh.ilja@gmail.com>
2017-06-13 14:35:39 +0300

    Add some entity with form validation.

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,20 +2,30 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Form\UserType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
     /**
      * @Route("/", name="homepage")
+     * @param Request $request
+     * @return Response
      */
     public function indexAction(Request $request)
     {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', array(
-            'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
-        ));
+        $form = $this->createForm(UserType::class, null, [
+            'method' => $request->getMethod(),
+        ]);
+        $form->handleRequest($request);
+
+        if (!$form->isValid()) {
+            return new Response('Error', 400);
+        }
+
+        return new Response('Success');
     }
 }

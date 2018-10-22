07aa5beeeb45d3d8f94cf9c5664ba21c4c8a34ab
Lesnykh Ilia <lesnykh.ilja@gmail.com>
2018-10-23 02:58:50 +0300

    Add test controller.

---
+++ src/Controller/WhateverController.php
@@ -0,0 +1,26 @@
+<?php
+/** @noinspection ALL */
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\Extension\Core\Type\FormType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class WhateverController extends Controller
+{
+    /**
+     * @Route("/")
+     */
+    public function index(Request $request): Response
+    {
+        $number = random_int(0, 100);
+
+        return new Response(
+            '<html><body>Lucky number: '.$number.'</body></html>'
+        );
+    }
+}

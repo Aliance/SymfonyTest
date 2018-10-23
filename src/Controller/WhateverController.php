33a48d175d4ce23ddab9d9575b39b1b68e9e9e9b
Lesnykh Ilia <lesnykh.ilja@gmail.com>
2018-10-23 14:36:27 +0300

    Handle form in controller.

--- src/Controller/WhateverController.php
+++ src/Controller/WhateverController.php
@@ -4,23 +4,44 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\Form\Extension\Core\Type\FormType;
+use App\Form\MyFormType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class WhateverController extends Controller
+class WhateverController extends AbstractController
 {
     /**
      * @Route("/")
      */
     public function index(Request $request): Response
     {
-        $number = random_int(0, 100);
+        $formType = MyFormType::class;
 
-        return new Response(
-            '<html><body>Lucky number: '.$number.'</body></html>'
-        );
+        $defaultFormData = null;
+
+        $formOptions = [
+            'method' => Request::METHOD_GET,
+        ];
+
+        $form = $this->createForm($formType, $defaultFormData, $formOptions);
+
+        $form = $this->container->get('form.factory')
+            ->createBuilder($formType, $defaultFormData, $formOptions)
+            ->getForm()
+        ;
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $formData = $form->getData();
+
+            $this->redirectToRoute('app_whatever_index');
+        }
+
+        return $this->render('form.html.twig', [
+            'form' => $form->createView(),
+        ]);
     }
 }
